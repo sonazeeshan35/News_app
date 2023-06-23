b0507fa0477af7eeb9c35a821fdbcfcb13c4014b
sonaz <sona_zeeshan35@yahoo.com>
2023-06-23 11:51:06 +0100

    blank app created

---
+++ News_app-api/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'News_app-api',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
